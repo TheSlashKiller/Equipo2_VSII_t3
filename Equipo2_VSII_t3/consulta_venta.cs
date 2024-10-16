100390c21643459945624fa3116c9cb5c3c57917
TheSlashKiller <Enrique_17_12@outlook.com>
2024-10-16 13:54:09 -0600

    Agregar archivos de proyecto.

---
+++ Equipo2_VSII_t3/consulta_venta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Equipo2_VSII_t3
+{
+    public partial class consulta_venta : Form
+    {
+        public consulta_venta()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Equipo2_VSII_t3/historial_tickets.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Equipo2_VSII_t3
+{
+    partial class historial_tickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // historial_tickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 528);
+            this.Name = "historial_tickets";
+            this.Text = "historial de tickets";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
